908d6e409f35edeaaf96061a72f2346eb5f44be6
loicbaillif <larmedeciel@gmail.com>
2022-07-24 00:48:21 +0200

    03/tablesLoops/init
    Tables and Loops: Initialization

---
+++ 03_tablesLoops/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Tables and Loops</title>
+</head>
+
+<body>
+<h1>Tables and Loops</h1>
+<hr/>
+<h2>Tables</h2>
+
+
+
+<hr/>
+<a href="../index.php">index</a>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <li><a href="./00_firstScript/index.php">Chapter 00: First Script</a></li>
         <li><a href="./01_variables/index.php">Chapter 01: Variables</a></li>
         <li><a href="./02_conditions/index.php">Chapter 02: Conditions</a></li>
-        <li><a href="./03/index.php">Chapter 03:</a></li>
+        <li><a href="./03_tablesLoops/index.php">Chapter 03: Tables and Loops</a></li>
         <li><a href="./04/index.php">Chapter 04:</a></li>
         <li><a href="./05/index.php">Chapter 05:</a></li>
         <li><a href="./06/index.php">Chapter 06:</a></li>
