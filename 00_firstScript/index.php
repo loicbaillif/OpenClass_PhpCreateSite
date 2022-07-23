7fe76dfb42854001c389ce5196cc63586e23b270
loicbaillif <larmedeciel@gmail.com>
2022-07-23 13:44:50 +0200

    00/firstScript/v2
    First echo

--- 00_firstScript/index.php
+++ 00_firstScript/index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First PHP Script</title>
+    <title>First PHP<?php // code ?></title>
 </head>
 
 <body>
-<h1>OpenClassrooms</h1>
+<h1 <?php // Some code to attribute dynamically a class ?>>OpenClassrooms</h1>
 <h2>First PHP Script</h2>
 <p>
     Some HTML code is included in this page <br/>
@@ -25,6 +25,7 @@
 
 <?php
 // Again and again PHP Code
+echo "First PHP text";
 ?>
 </body>
 </html>
