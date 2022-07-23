3886cb48f0bb152fabbb569369ed661eebdd7716
loicbaillif <larmedeciel@gmail.com>
2022-07-23 13:39:46 +0200

    00/firstScript/v1
    First PHP Script, and upload on website

--- 00_firstScript/index.php
+++ 00_firstScript/index.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First PHP Script</title>
 </head>
+
 <body>
-    <h1>OpenClassrooms</h1>
-    <h2>First PHP Script</h2>
-    <p>
-        Hello <!-- Some first name to introduce here --> !
-    </p>
+<h1>OpenClassrooms</h1>
+<h2>First PHP Script</h2>
+<p>
+    Some HTML code is included in this page <br/>
+    Hello <?php /* Some first name to introduce here */ ?> !
+    Few tests here-below
+</p>
+
+<ul>
+    <li style="color: blue;">Blue text</li>
+    <li style="color: red;">Red text</li>
+    <li style="color: green;">Green text</li>
+</ul>
+
+<?php
+// Again and again PHP Code
+?>
 </body>
 </html>
