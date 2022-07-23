83de8b9940ad5b7034e6160dafb42a04fa0e17cf
loicbaillif <larmedeciel@gmail.com>
2022-07-23 16:43:09 +0200

    01/variables/init
    Chapter 01: Variables
    Initialization

---
+++ 01_variables/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>PHP Variables</title>
+</head>
+
+<body>
+<h1>PHP Variables</h1>
+<hr />
+
+
+<hr/>
+<a href="../index.php">index</a>
+</body>
+
+</html>
