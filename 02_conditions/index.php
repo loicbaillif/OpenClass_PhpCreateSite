d28d3af0a0f6697058d4f4a9c1adcf6b0ba8f8b3
loicbaillif <larmedeciel@gmail.com>
2022-07-23 22:11:42 +0200

    02/conditions/v1
    if ... elseif ... else : First example

--- 02_conditions/index.php
+++ 02_conditions/index.php
@@ -12,8 +12,30 @@
 <body>
 <h1>Conditions</h1>
 <hr />
+<h2>if ... else (a.k.a. bread ... butter)</h2>
+<p>
+
+<?php
+$is_enabled = true;
+if ($is_enabled) {
+    echo 'You are authorized to access this website.✅<br/>' . PHP_EOL;
+} else {
+    echo 'Access denied. ❌<br/>' . PHP_EOL;
+}
+
+$user_satisfied = "Maybe";
+if ($user_satisfied == "Yes") {
+    echo 'I\'m glad you are a satisfied user.<br />' . PHP_EOL;
+} elseif ($user_satisfied == "No") {
+    echo 'I will do my best to satisfy you in the future.<br/>' . PHP_EOL;
+} else {
+    echo 'Choosing means eliminating<br/>' . PHP_EOL;
+}
 
 
+?>
+
+</p>
 
 
 
