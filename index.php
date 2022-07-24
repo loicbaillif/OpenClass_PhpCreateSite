5a1b2a9ce8aa0a9cc5d7cd1abd29995e71ed70ee
loicbaillif <larmedeciel@gmail.com>
2022-07-24 11:57:41 +0200

    04/arrays/init
    Structure data in arrays
    Chapter initialization

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <li><a href="./01_variables/index.php">Chapter 01: Variables</a></li>
         <li><a href="./02_conditions/index.php">Chapter 02: Conditions</a></li>
         <li><a href="./03_tablesLoops/index.php">Chapter 03: Tables and Loops</a></li>
-        <li><a href="./04/index.php">Chapter 04:</a></li>
+        <li><a href="./04_arrays/index.php">Chapter 04: Arrays</a></li>
         <li><a href="./05/index.php">Chapter 05:</a></li>
         <li><a href="./06/index.php">Chapter 06:</a></li>
         <li><a href="./07/index.php">Chapter 07:</a></li>
