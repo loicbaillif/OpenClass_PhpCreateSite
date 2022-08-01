d0e9e9eb993840f42ee7ceb4504dce395ac1efd1
loicbaillif <larmedeciel@gmail.com>
2022-08-01 17:56:11 +0200

    06/organize/v6
    Two variable files created to re-organize code
    Next: create dedicated file for functions

---
+++ 99/varRecipes.php
@@ -0,0 +1,35 @@
+<?php
+$recipes = [
+    [
+        'title' => 'pudding',
+        'recipe' => 'Light and typically english, a nice dessert',
+        'authorEmail' => 'user323@somemail.com',
+        'isEnabled' => true,
+    ],
+    [
+        'title' => 'pizza',
+        'recipe' => 'fresh and basic ingredients, fun to eat. Kids love it',
+        'authorEmail' => 'vivitalia@somemail.com',
+        'isEnabled' => false,
+    ],
+    [
+        'title' => 'tika masala',
+        'recipe' => 'indian-based, adopted in the UK.',
+        'authorEmail' => 'userfromuk@somemail.com',
+        'isEnabled' => true,
+    ],
+    [
+        'title' => 'pancake',
+        'recipe' => 'For top class breakfast. Flour, eggs, milk',
+        'authorEmail' => 'userfromuk@somemail.com',
+        'isEnabled' => false,
+    ],
+    [
+        'title' => 'gazpacho',
+        'recipe' => 'All you need is tomatoes, olive oil, cucumber.',
+        'authorEmail' => 'user323@somemail.com',
+        'isEnabled' => true,
+    ],
+];
+
+?>
---
+++ 99/varUsers.php
@@ -0,0 +1,21 @@
+<?php
+$users = [
+    [
+        'firstName' => 'Rémi',
+        'lastName' => 'Fasola',
+        'eMail' => 'user323@somemail.com',
+        'age' => 42,
+    ],
+    [
+        'firstName' => 'Clive',
+        'lastName' => 'Brown',
+        'eMail' => 'userfromuk@somemail.com',
+        'age' => 42,
+    ],
+    [
+        'firstName' => 'Carla',
+        'lastName' => 'Pizza',
+        'eMail' => 'vivitalia@somemail.com',
+        'age' => 42,
+    ],
+];
