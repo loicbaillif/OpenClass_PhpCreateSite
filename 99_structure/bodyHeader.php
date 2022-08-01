dc38ff4627dee0e7b44c5f32a8d84e22112b39fd
loicbaillif <larmedeciel@gmail.com>
2022-08-01 12:37:10 +0200

    06/organize/v2
    Create bodyHeader, which includes the navbar, and the main title

---
+++ 99_structure/bodyHeader.php
@@ -0,0 +1,13 @@
+<header>
+    <nav id="top-menu">
+        <div class="element-menu">
+            <h4>OpenClassrooms: Create dynamic webpages with PHP</h4>
+            <ul class="navbar">
+                <li class="navbar"><a href="../index.php">Home</a></li>
+                <li class="navbar"><a href="https://github.com/loicbaillif/OC_PHP_intro">Git Repository</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<h1><?php echo $mainTitle; ?></h1>
+<hr/>
