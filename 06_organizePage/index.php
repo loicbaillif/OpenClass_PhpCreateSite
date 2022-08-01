b9e5675e361d0ba8ccec74f72194c5d11d3930c3
loicbaillif <larmedeciel@gmail.com>
2022-08-01 12:38:37 +0200

    06/organize/v4
    Page reconstruction, to include created parts:
    head.php ; bodyHeader.php ; bodyFooter.php

--- 06_organizePage/index.php
+++ 06_organizePage/index.php
@@ -1,25 +1,24 @@
+<?php
+// Document variables:
+$chapter = 06;
+$pageTitle = 'Organize page';
+$mainTitle = 'Organize page in functional blocks';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
-    <title>Organize page</title>
-</head>
+<?php include('../99_structure/head.php'); ?>
 
 <body>
-<h1>Organize page in functional blocks</h1>
-<hr/>
+<?php include('../99_structure/bodyHeader.php'); ?>
 
 
 
 
 
 
-<hr/>
-<a href="../index.php">index</a>
+<?php include('../99_structure/bodyFooter.php'); ?>
 </body>
 
 </html>
